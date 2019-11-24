2b3f67ecccdfb0548e37bc09e9f202dfaa034a94
Shahin Sabooni <longlonghands@outlook.com>
2019-11-24 20:17:20 +0330

    adding pool usage to make a better performance + some refactorings

--- statement.go
+++ statement.go
@@ -48,12 +48,20 @@ type Statement interface {
 	Limit(limit interface{}) Statement
 	Offset(offset interface{}) Statement
 
+	// Paginate provides an easy way to set both offset and limit
 	Paginate(page, pageSize int) Statement
 
 	Join(table, on string) Statement
 	LeftJoin(table, on string) Statement
 	RightJoin(table, on string) Statement
 	FullJoin(table, on string) Statement
+
+	Returning(expr string) Statement
+	With(queryName string, query Statement) Statement
+	Expr(expr string, args ...interface{}) Statement
+	SubQuery(prefix, suffix string, query Statement) Statement
+	Union(all bool, query Statement) Statement
+	Clause(expr string, args ...interface{}) Statement
 }
 
 type statement struct {
@@ -73,196 +81,6 @@ type statementPart struct {
 	hasExpr  bool
 	argLen   int
 }
-
-func getBuffer() *bytebufferpool.ByteBuffer {
-	return bytebufferpool.Get()
-}
-
-func putBuffer(buf *bytebufferpool.ByteBuffer) {
-	bytebufferpool.Put(buf)
-}
-
-func newStmt() *statement {
-	return &statement{
-		parts: make([]statementPart, 0, 8),
-	}
-}
-
-func getStmt(d Dialect) *statement {
-	stmt := newStmt()
-	stmt.dialect = d
-	stmt.buffer = getBuffer()
-	return stmt
-}
-
-func reuseStmt(stmt *statement) {
-	stmt.parts = stmt.parts[:0]
-	if len(stmt.args) > 0 {
-		for n := range stmt.args {
-			stmt.args[n] = nil
-		}
-		stmt.args = stmt.args[:0]
-	}
-	if len(stmt.dest) > 0 {
-		for n := range stmt.dest {
-			stmt.dest[n] = nil
-		}
-		stmt.dest = stmt.dest[:0]
-	}
-	putBuffer(stmt.buffer)
-	stmt.buffer = nil
-	if stmt.sql != nil {
-		putBuffer(stmt.sql)
-	}
-	stmt.sql = nil
-
-	// stmtPool.Put(stmt)
-}
-
-// addPart adds a clause or expression to a statement.
-func (stmt *statement) addPart(pos int, clause, expr string, args []interface{}, sep string) (index int) {
-	// Remember the position
-	stmt.position = pos
-
-	argLen := len(args)
-	bufLow := len(stmt.buffer.B)
-	index = len(stmt.parts)
-	argTail := 0
-
-	addNew := true
-	addClause := clause != ""
-
-	// Find the position to insert a chunk to
-loop:
-	for i := index - 1; i >= 0; i-- {
-		chunk := &stmt.parts[i]
-		index = i
-		switch {
-		// See if an existing chunk can be extended
-		case chunk.position == pos:
-			// Do nothing if a clause is already there and no expressions are to be added
-			if expr == "" {
-				// See if arguments are to be updated
-				if argLen > 0 {
-					copy(stmt.args[len(stmt.args)-argTail-chunk.argLen:], args)
-				}
-				return i
-			}
-			// Write a separator
-			if chunk.hasExpr {
-				stmt.buffer.WriteString(sep)
-			} else {
-				stmt.buffer.WriteString(" ")
-			}
-			if chunk.bufHigh == bufLow {
-				// Do not add a chunk
-				addNew = false
-				// UpdateUser the existing one
-				stmt.buffer.WriteString(expr)
-				chunk.argLen += argLen
-				chunk.bufHigh = len(stmt.buffer.B)
-				chunk.hasExpr = true
-			} else {
-				// Do not add a clause
-				addClause = false
-				index = i + 1
-			}
-			break loop
-		// No existing chunks of this type
-		case chunk.position < pos:
-			index = i + 1
-			break loop
-		default:
-			argTail += chunk.argLen
-		}
-	}
-
-	if addNew {
-		// Insert a new chunk
-		if addClause {
-			stmt.buffer.WriteString(clause)
-			if expr != "" {
-				stmt.buffer.WriteString(" ")
-			}
-		}
-		stmt.buffer.WriteString(expr)
-
-		if cap(stmt.parts) == len(stmt.parts) {
-			chunks := make([]statementPart, len(stmt.parts), cap(stmt.parts)*2)
-			copy(chunks, stmt.parts)
-			stmt.parts = chunks
-		}
-
-		chunk := statementPart{
-			position: pos,
-			bufLow:   bufLow,
-			bufHigh:  len(stmt.buffer.B),
-			argLen:   argLen,
-			hasExpr:  expr != "",
-		}
-
-		stmt.parts = append(stmt.parts, chunk)
-		if index < len(stmt.parts)-1 {
-			copy(stmt.parts[index+1:], stmt.parts[index:])
-			stmt.parts[index] = chunk
-		}
-	}
-
-	// Insert query arguments
-	if argLen > 0 {
-		stmt.args = insertAt(stmt.args, args, len(stmt.args)-argTail)
-	}
-	stmt.Invalidate()
-
-	return index
-}
-
-// join adds a join clause to a SELECT statement
-func (stmt *statement) join(joinType, table, on string) (index int) {
-	buf := bytebufferpool.Get()
-	buf.WriteString(joinType)
-	buf.WriteString(table)
-	buf.Write(joinOn)
-	buf.WriteString(on)
-	buf.WriteByte(')')
-
-	index = stmt.addPart(posFrom, "", bufferToString(&buf.B), nil, " ")
-
-	bytebufferpool.Put(buf)
-
-	return index
-}
-
-var (
-	space            = []byte{' '}
-	placeholder      = []byte{'?'}
-	placeholderComma = []byte{'?', ','}
-	joinOn           = []byte{' ', 'O', 'N', ' ', '('}
-)
-
-const (
-	_        = iota
-	posStart = 100 * iota
-	posWith
-	posInsert
-	posInsertFields
-	posValues
-	posDelete
-	posUpdate
-	posSet
-	posSelect
-	posInto
-	posFrom
-	posWhere
-	posGroupBy
-	posHaving
-	posUnion
-	posOrderBy
-	posLimit
-	posOffset
-	posReturning
-	posEnd
-)
 
 /*
 New initializes a SQL statement builder instance with an arbitrary verb.
@@ -632,6 +450,21 @@ func (stmt *statement) Offset(offset interface{}) Statement {
 	return stmt
 }
 
+// Paginate provides an easy way to set both offset and limit
+func (stmt *statement) Paginate(page, pageSize int) Statement {
+	if page < 1 {
+		page = 1
+	}
+	if pageSize < 1 {
+		pageSize = 1
+	}
+	if page > 1 {
+		stmt.Offset((page - 1) * pageSize)
+	}
+	stmt.Limit(pageSize)
+	return stmt
+}
+
 /*
 Join adds an INNERT JOIN clause to SELECT statement
 */
@@ -661,21 +494,6 @@ FullJoin adds a FULL OUTER JOIN clause to SELECT statement
 */
 func (stmt *statement) FullJoin(table, on string) Statement {
 	stmt.join("FULL JOIN ", table, on)
-	return stmt
-}
-
-// Paginate provides an easy way to set both offset and limit
-func (stmt *statement) Paginate(page, pageSize int) Statement {
-	if page < 1 {
-		page = 1
-	}
-	if pageSize < 1 {
-		pageSize = 1
-	}
-	if page > 1 {
-		stmt.Offset((page - 1) * pageSize)
-	}
-	stmt.Limit(pageSize)
 	return stmt
 }
 
---
+++ statement_impl.go
@@ -0,0 +1,201 @@
+package sqlbuilder
+
+import (
+	"sync"
+
+	"github.com/valyala/bytebufferpool"
+)
+
+var (
+	stmtPool = sync.Pool{New: newStmt}
+)
+
+func getBuffer() *bytebufferpool.ByteBuffer {
+	return bytebufferpool.Get()
+}
+
+func putBuffer(buf *bytebufferpool.ByteBuffer) {
+	bytebufferpool.Put(buf)
+}
+
+func newStmt() interface{} {
+	return &statement{
+		parts: make([]statementPart, 0, 8),
+	}
+}
+
+func getStmt(d Dialect) *statement {
+	stmt := stmtPool.Get().(*statement)
+	stmt.dialect = d
+	stmt.buffer = getBuffer()
+	return stmt
+}
+
+func reuseStmt(stmt *statement) {
+	stmt.parts = stmt.parts[:0]
+	if len(stmt.args) > 0 {
+		for n := range stmt.args {
+			stmt.args[n] = nil
+		}
+		stmt.args = stmt.args[:0]
+	}
+	if len(stmt.dest) > 0 {
+		for n := range stmt.dest {
+			stmt.dest[n] = nil
+		}
+		stmt.dest = stmt.dest[:0]
+	}
+	putBuffer(stmt.buffer)
+	stmt.buffer = nil
+	if stmt.sql != nil {
+		putBuffer(stmt.sql)
+	}
+	stmt.sql = nil
+
+	stmtPool.Put(stmt)
+}
+
+var (
+	space            = []byte{' '}
+	placeholder      = []byte{'?'}
+	placeholderComma = []byte{'?', ','}
+	joinOn           = []byte{' ', 'O', 'N', ' ', '('}
+)
+
+const (
+	_        = iota
+	posStart = 100 * iota
+	posWith
+	posInsert
+	posInsertFields
+	posValues
+	posDelete
+	posUpdate
+	posSet
+	posSelect
+	posInto
+	posFrom
+	posWhere
+	posGroupBy
+	posHaving
+	posUnion
+	posOrderBy
+	posLimit
+	posOffset
+	posReturning
+	posEnd
+)
+
+// addPart adds a clause or expression to a statement.
+func (stmt *statement) addPart(pos int, clause, expr string, args []interface{}, sep string) (index int) {
+	// Remember the position
+	stmt.position = pos
+
+	argLen := len(args)
+	bufLow := len(stmt.buffer.B)
+	index = len(stmt.parts)
+	argTail := 0
+
+	addNew := true
+	addClause := clause != ""
+
+	// Find the position to insert a chunk to
+loop:
+	for i := index - 1; i >= 0; i-- {
+		chunk := &stmt.parts[i]
+		index = i
+		switch {
+		// See if an existing chunk can be extended
+		case chunk.position == pos:
+			// Do nothing if a clause is already there and no expressions are to be added
+			if expr == "" {
+				// See if arguments are to be updated
+				if argLen > 0 {
+					copy(stmt.args[len(stmt.args)-argTail-chunk.argLen:], args)
+				}
+				return i
+			}
+			// Write a separator
+			if chunk.hasExpr {
+				stmt.buffer.WriteString(sep)
+			} else {
+				stmt.buffer.WriteString(" ")
+			}
+			if chunk.bufHigh == bufLow {
+				// Do not add a chunk
+				addNew = false
+				// UpdateUser the existing one
+				stmt.buffer.WriteString(expr)
+				chunk.argLen += argLen
+				chunk.bufHigh = len(stmt.buffer.B)
+				chunk.hasExpr = true
+			} else {
+				// Do not add a clause
+				addClause = false
+				index = i + 1
+			}
+			break loop
+		// No existing chunks of this type
+		case chunk.position < pos:
+			index = i + 1
+			break loop
+		default:
+			argTail += chunk.argLen
+		}
+	}
+
+	if addNew {
+		// Insert a new chunk
+		if addClause {
+			stmt.buffer.WriteString(clause)
+			if expr != "" {
+				stmt.buffer.WriteString(" ")
+			}
+		}
+		stmt.buffer.WriteString(expr)
+
+		if cap(stmt.parts) == len(stmt.parts) {
+			chunks := make([]statementPart, len(stmt.parts), cap(stmt.parts)*2)
+			copy(chunks, stmt.parts)
+			stmt.parts = chunks
+		}
+
+		chunk := statementPart{
+			position: pos,
+			bufLow:   bufLow,
+			bufHigh:  len(stmt.buffer.B),
+			argLen:   argLen,
+			hasExpr:  expr != "",
+		}
+
+		stmt.parts = append(stmt.parts, chunk)
+		if index < len(stmt.parts)-1 {
+			copy(stmt.parts[index+1:], stmt.parts[index:])
+			stmt.parts[index] = chunk
+		}
+	}
+
+	// Insert query arguments
+	if argLen > 0 {
+		stmt.args = insertAt(stmt.args, args, len(stmt.args)-argTail)
+	}
+	stmt.Invalidate()
+
+	return index
+}
+
+// join adds a join clause to a SELECT statement
+func (stmt *statement) join(joinType, table, on string) (index int) {
+	buf := bytebufferpool.Get()
+	buf.WriteString(joinType)
+	buf.WriteString(table)
+	buf.Write(joinOn)
+	buf.WriteString(on)
+	buf.WriteByte(')')
+
+	index = stmt.addPart(posFrom, "", bufferToString(&buf.B), nil, " ")
+
+	bytebufferpool.Put(buf)
+
+	return index
+}
